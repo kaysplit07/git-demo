641979dbbd6bcf146f385f6d55f035a89fea2879
kaysplit07 <62314720+kaysplit07@users.noreply.github.com>
2024-09-03 17:30:15 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,3 +1,5 @@
-Error: creating Windows Virtual Machine (Subscription: "***"
-│ Resource Group Name: "6425-dev-eus2-sgs-rg"
-│ Virtual Machine Name: "AZUSE-DEFDV751"): performing CreateOrUpdate: unexpected status 400 (400 Bad Request) with error: InvalidParameter: The property 'securityProfile.encryptionAtHost' is not valid because the 'Microsoft.Compute/EncryptionAtHost' feature is not enabled for this subscription.
+│ Error: unable to build authorizer for Resource Manager API: could not configure AzureCli Authorizer: could not parse Azure CLI version: launching Azure CLI: exec: "az": executable file not found in $PATH
+│ 
+│   with module.availability_set_main.provider["registry.terraform.io/hashicorp/azurerm"],
+│   on ../availability-set/main.tf line 13, in provider "azurerm":
+│   13: provider "azurerm" {
