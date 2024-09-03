7b1d00355e9fe851335bbc09bcaa67e35dc83822
kaysplit07 <62314720+kaysplit07@users.noreply.github.com>
2024-09-03 16:40:32 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,3 +1,3 @@
-output "vm_names" {
-  value = [for vm in azurerm_windows_virtual_machine.main : vm.name]
-}
+Error: creating Windows Virtual Machine (Subscription: "***"
+│ Resource Group Name: "6425-dev-eus2-sgs-rg"
+│ Virtual Machine Name: "AZUSE-DEFDV751"): performing CreateOrUpdate: unexpected status 400 (400 Bad Request) with error: InvalidParameter: The property 'securityProfile.encryptionAtHost' is not valid because the 'Microsoft.Compute/EncryptionAtHost' feature is not enabled for this subscription.
