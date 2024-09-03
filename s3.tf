a34836b79773efe920e30d889aff490e60d30a10
kaysplit07 <62314720+kaysplit07@users.noreply.github.com>
2024-09-03 14:59:02 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,1 +1,5 @@
-Choose a starting address and size within your IPv4 address range: 10.82.58.192/26 (10.82.58.192 - 10.82.58.255)
+│ Error: unable to assume default computer name "computer_name" can be at most 15 characters, got 18. Please adjust the "name", or specify an explicit "computer_name"
+│ 
+│   with azurerm_windows_virtual_machine.main["0"],
+│   on main.tf line 665, in resource "azurerm_windows_virtual_machine" "main":
+│  665: resource "azurerm_windows_virtual_machine" "main" {
