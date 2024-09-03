702b684398c4f7b5a981a0282e1068abf4da6076
kaysplit07 <62314720+kaysplit07@users.noreply.github.com>
2024-09-03 15:45:39 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,3 @@
-│ Error: unable to assume default computer name "computer_name" can be at most 15 characters, got 18. Please adjust the "name", or specify an explicit "computer_name"
-│ 
-│   with azurerm_windows_virtual_machine.main["0"],
-│   on main.tf line 665, in resource "azurerm_windows_virtual_machine" "main":
-│  665: resource "azurerm_windows_virtual_machine" "main" {
+output "vm_names" {
+  value = [for vm in azurerm_windows_virtual_machine.main : vm.name]
+}
