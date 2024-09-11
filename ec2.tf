a719b3ca8bcaf9137e2b5944386a7462176174ac
kaysplit07 <62314720+kaysplit07@users.noreply.github.com>
2024-09-11 14:30:53 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,6 @@
 name: 'Deploy Subnet'
 run-name: '${{inputs.environment}}:Deploying Subnet - ${{github.actor}}'
+
 on:
   workflow_dispatch:
     inputs:
@@ -8,21 +9,21 @@ on:
         description: Location
         required: true
         options:
-        - 'Select'
-        - eastus2
-        - centralus
-        - uksouth
-        - ukwest
+          - 'Select'
+          - eastus2
+          - centralus
+          - uksouth
+          - ukwest
       environment:
         type: choice
         description: Environment
         required: true
         options:
-        - 'Select'
-        - dev
-        - qa
-        - uat
-        - Prod
+          - 'Select'
+          - dev
+          - qa
+          - uat
+          - Prod
       purpose:
         type: string
         description: Purpose for Subnet Name [3-5 unique char, CAN include Numbers] ex-kub1, wapp2, func3, etc...
@@ -33,9 +34,9 @@ on:
         required: true
         default: "small"
         options:
-        - 'small'
-        - "medium"
-        - "large"
+          - 'small'
+          - "medium"
+          - "large"
       subnetNumber:
         type: string
         description: Number for Subnet to differentiate multiple subnets of the same purpose (e.g., 01, 02)
@@ -44,9 +45,8 @@ on:
       adtEmailAddress:
         type: string
         description: ADT Email Address (specify as 'first.last@nationalgrid.com'. NO '@uk.' or '@us.' in Domain)
-        required: true
-# env:
-#   ROOT_PATH: 'terraform/initialsetup'
+        required: true  
+
 jobs:
   Add-Subnet:
     name: 'Add Subnet'
@@ -57,20 +57,87 @@ jobs:
       ARM_SUBSCRIPTION_ID: ${{secrets.AZURE_SUBSCRIPTION_ID}}
     runs-on: ubuntu-latest
     environment: ${{inputs.environment}}
-    # Use the Bash shell regardless whether the GitHub Actions runner is ubuntu-latest, macos-latest, or windows-latest
     defaults:
       run:
         shell: bash
     steps:
-    - uses: actions/checkout@v2
-    - name: Az login
-      uses: azure/login@v2
-      with:
-        creds: '{"clientId":"${{ secrets.AZURE_CLIENT_ID }}","clientSecret":"${{ secrets.AZURE_CLIENT_SECRET }}","subscriptionId":"${{ secrets.AZURE_SUBSCRIPTION_ID }}","tenantId":"${{ secrets.AZURE_TENANT_ID }}"}'
-        enable-AzPSSession: true
-    - name: Azure PowerShell Action
-      id: 'AddSubnet'
-      uses: azure/powershell@v2
-      with:
-        azPSVersion: latest
-        inlineScript: "$subscription = (Set-AzContext -Subscription ${{ secrets.AZURE_SUBSCRIPTION_ID }})\n$subscriptionName = $subscription.Subscription.Name\n$bu = $subscriptionName.split('-')[1]\n$env = $subscriptionName.split('-')[2]\n$nn = $subscriptionName.split('-')[3]\n$tenantId = $subscription.Tenant.Id\nif (\"${{inputs.location}}\" -eq \"eastus2\") {\n  $region = \"eus2\"\n  $geo = \"US\"\n}\nelseif (\"${{inputs.location}}\" -eq \"centralus\") {\n  $region = \"cus\"\n  $geo = \"US\"\n}\nelseif (\"${{inputs.location}}\" -eq \"uksouth\") {\n  $region = \"uks\"\n  $geo = \"UK\"\n}\nelse{\n  $region = \"ukw\"\n  $geo = \"UK\"\n}\n  #magic___^_^___line\n$spokenetworkName = $bu + \"-\" + $env + \"-\" + $region + \"-spokenetwork-rg\"\nWrite-Output \"SPOKENETWORK NAME: $spokenetworkName\" \n  #magic___^_^___line\n$vnetName = $bu + \"-\" + $env + \"-\" + $region + \"-vnet-\" + $nn\nWrite-Output \"VNET NAME: $vnetName\"\n  #magic___^_^___line\n# Modified subnet name to include purpose and subnetNumber\n$subnetName = \"lz\" + $bu + \"-\" + $env + \"-\" + $region + \"-\" + \"${{inputs.purpose}}\" + \"-snet-${{inputs.subnetNumber}}\"\n$subnetName_Lowercase = $subnetName.ToLower()\nWrite-Output \"SUBNET NAME: $subnetName_Lowercase\" \n  #magic___^_^___line\n$data = @{\n    \"geo\"                   = $geo\n    \"location\"              = $region\n    \"VNetName\"              = $vnetName\n    \"SubnetName\"            = $subnetName_Lowercase\n    \"subnetSize\"            = \"${{inputs.subnetSize}}\"\n    \"SubscriptionName\"      = $subscriptionName\n    \"resourceGroupName\"     = $spokenetworkName\n    \"requesterEmailAddress\" = \"${{inputs.adtEmailAddress}}\"\n}\n$spAppId = Get-AzKeyVaultSecret -VaultName 'ng-prd-eus2-kv-01' -Name \"PLAT-logicAppAPI-prod-01-SPN-appId\" -AsPlainText\n$Password = Get-AzKeyVaultSecret -VaultName 'ng-prd-eus2-kv-01' -Name \"PLAT-logicAppAPI-prod-01-SPN-Password\"\n$credential = New-Object System.Management.Automation.PsCredential($spAppId, $Password.SecretValue)\nConnect-AzAccount -ServicePrincipal -Credential $credential -Tenant $tenantId\n$accessToken = Get-AzAccessToken -ResourceUrl 'https://management.core.windows.net/'\n    #magic___^_^___line\n$headers = New-Object \"System.Collections.Generic.Dictionary[[String],[String]]\"\n$headers.Add(\"Authorization\", \"Bearer \" + $accessToken.Token)\n$headers.Add(\"Content-Type\", \"application/json\")\n$json = $data | ConvertTo-Json\nWrite-Host \"==> Sent\"\nWrite-Host $json\nWrite-Host \"==> EOM\"\n    #magic___^_^___line\n$logicAppInfo = Invoke-RestMethod 'https://prod-24.eastus2.logic.azure.com:443/workflows/66492a0351fe422ea527a48f797a59d4/triggers/manual/paths/invoke?api-version=2016-10-01' -Method 'POST' -Headers $headers -Body $json\n$logicAppInfo | ConvertTo-Json\nWrite-Host \"==> Received\"\nWrite-Host $logicAppInfo\nWrite-Host \"==> EOM\"\n"
+      - uses: actions/checkout@v2
+
+      - name: Az login
+        uses: azure/login@v2
+        with: 
+            creds: '{"clientId":"${{ secrets.AZURE_CLIENT_ID }}","clientSecret":"${{ secrets.AZURE_CLIENT_SECRET }}","subscriptionId":"${{ secrets.AZURE_SUBSCRIPTION_ID }}","tenantId":"${{ secrets.AZURE_TENANT_ID }}"}'
+            enable-AzPSSession: true
+
+      - name: Azure PowerShell Action
+        id: 'AddSubnet'
+        uses: azure/powershell@v2
+        with:
+          azPSVersion: latest
+          inlineScript: |
+            $subscription = (Set-AzContext -Subscription ${{ secrets.AZURE_SUBSCRIPTION_ID }})
+            $subscriptionName = $subscription.Subscription.Name
+            $bu = $subscriptionName.split('-')[1]
+            $env = $subscriptionName.split('-')[2]
+            $nn = $subscriptionName.split('-')[3]
+            $tenantId = $subscription.Tenant.Id
+
+            if ("${{inputs.location}}" -eq "eastus2") {
+              $region = "eus2"
+              $geo = "US"
+            }
+            elseif ("${{inputs.location}}" -eq "centralus") {
+              $region = "cus"
+              $geo = "US"
+            }
+            elseif ("${{inputs.location}}" -eq "uksouth") {
+              $region = "uks"
+              $geo = "UK"
+            }
+            else{
+              $region = "ukw"
+              $geo = "UK"
+            }
+
+            $spokenetworkName = $bu + "-" + $env + "-" + $region + "-spokenetwork-rg"
+            Write-Output "SPOKENETWORK NAME: $spokenetworkName" 
+
+            $vnetName = $bu + "-" + $env + "-" + $region + "-vnet-" + $nn
+            Write-Output "VNET NAME: $vnetName"
+
+            # Modified subnet name to include purpose and subnetNumber
+            $subnetName = "lz" + $bu + "-" + $env + "-" + $region + "-" + "${{inputs.purpose}}" + "-snet-${{inputs.subnetNumber}}"
+            $subnetName_Lowercase = $subnetName.ToLower()
+            Write-Output "SUBNET NAME: $subnetName_Lowercase" 
+
+            $data = @{
+                "geo"                   = $geo
+                "location"              = $region
+                "VNetName"              = $vnetName
+                "SubnetName"            = $subnetName_Lowercase
+                "subnetSize"            = "${{inputs.subnetSize}}"
+                "SubscriptionName"      = $subscriptionName
+                "resourceGroupName"     = $spokenetworkName
+                "requesterEmailAddress" = "${{inputs.adtEmailAddress}}"
+            }
+            
+            # Retrieve API secrets from KeyVault
+            $spAppId = Get-AzKeyVaultSecret -VaultName 'ng-prd-eus2-kv-01' -Name "PLAT-logicAppAPI-prod-01-SPN-appId" -AsPlainText
+            $Password = Get-AzKeyVaultSecret -VaultName 'ng-prd-eus2-kv-01' -Name "PLAT-logicAppAPI-prod-01-SPN-Password"
+            $credential = New-Object System.Management.Automation.PsCredential($spAppId, $Password.SecretValue)
+            Connect-AzAccount -ServicePrincipal -Credential $credential -Tenant $tenantId
+            $accessToken = Get-AzAccessToken -ResourceUrl 'https://management.core.windows.net/'
+
+            $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+            $headers.Add("Authorization", "Bearer " + $accessToken.Token)
+            $headers.Add("Content-Type", "application/json")
+            $json = $data | ConvertTo-Json
+            Write-Host "==> Sent"
+            Write-Host $json
+            Write-Host "==> EOM"
+
+            $logicAppInfo = Invoke-RestMethod 'https://prod-24.eastus2.logic.azure.com:443/workflows/66492a0351fe422ea527a48f797a59d4/triggers/manual/paths/invoke?api-version=2016-10-01' -Method 'POST' -Headers $headers -Body $json
+            $logicAppInfo | ConvertTo-Json
+            Write-Host "==> Received"
+            Write-Host $logicAppInfo
+            Write-Host "==> EOM"
