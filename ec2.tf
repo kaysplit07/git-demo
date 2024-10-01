7f9fcf2baf7f9351433ae8047a6d84f09ab86723
kaysplit07 <62314720+kaysplit07@users.noreply.github.com>
2024-10-01 12:47:23 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,84 +1,243 @@
-$subscription = (Set-AzContext -Subscription ${{ secrets.AZURE_SUBSCRIPTION_ID }})
-$subscriptionName = $subscription.Subscription.Name
-$bu = $subscriptionName.split('-')[1]
-$env = $subscriptionName.split('-')[2]
-$nn = $subscriptionName.split('-')[3]
-$tenantId = $subscription.Tenant.Id
+provider "azurerm" {
+  features {}
+}
+data "azurerm_subscription" "current" {}  // read the current subscription info
 
-if ("${{inputs.location}}" -eq "eastus2") {
-  $region = "eus2"
-  $geo = "US"
+data "azurerm_client_config" "clientconfig" {} // read the current client config
+
+locals {
+  get_data= csvdecode(file("../parameters.csv"))
+ // define data for naming standards 
+ naming = {
+   bu = lower(split("-", data.azurerm_subscription.current.display_name)[1])  // read app/bu from the subscription data block
+   environment = lower(split("-", data.azurerm_subscription.current.display_name)[2])  // read environment from subscription data block
+   locations = var.location
+   nn                       = lower(split("-", data.azurerm_subscription.current.display_name)[3])
+   subscription_name        = data.azurerm_subscription.current.display_name
+   subscription_id          = data.azurerm_subscription.current.id
+ }
+ env_location = {
+    env_abbreviation       = var.environment_map[local.naming.environment]
+    locations_abbreviation = var.location_map[local.naming.locations]
+      }
+  # vnet_name = {
+  #   for index, inst in local.get_data : index => inst {
+  #     vnet_name = (lookup(inst, "vnet_name", null) != null && lookup(inst, "vnet_name", "") != "") ? inst.vnet_name : join("-", [local.naming.bu, local.naming.environment, local.naming_map.location_abbreviation[unique_id].location_abbreviation, "vnet", local.naming.nn])
+  #     vnet_rg   = (lookup(inst, "vnet_resource_group", null) != null && lookup(inst, "vnet_resource_group", "") != "") ? inst.vnet_resource_group : join("-", [local.naming.bu, local.naming.environment, local.naming_map.location_abbreviation[unique_id].location_abbreviation, "spokenetwork-rg"])
+  #   }
+
+  # }
+ purpose = var.RGname!="" ? lower(split("-",var.RGname)[3]) : var.purpose
+# subnet = var.subnet
 }
-elseif ("${{inputs.location}}" -eq "centralus") {
-  $region = "cus"
-  $geo = "US"
+
+data "azurerm_resource_group" "rg" {
+  for_each = { for inst in local.get_data : inst.unique_id => inst }
+  name     = join("", [local.naming.bu, "-", local.naming.environment, "-", local.env_location.locations_abbreviation, "-", local.purpose, "-rg"]) 
 }
-elseif ("${{inputs.location}}" -eq "uksouth") {
-  $region = "uks"
-  $geo = "UK"
+
+output "resource_group_name" {
+  value = data.azurerm_resource_group.rg
 }
-else {
-  $region = "ukw"
-  $geo = "UK"
+
+
+data "azurerm_virtual_network" "vnet" {
+  for_each = { for inst in local.get_data : inst.unique_id => inst }
+   name = (lookup(each.value, "vnet_name", null) != null && lookup(each.value, "vnet_name", "") != "") ? each.value.vnet_name : join("-", [local.naming.bu, local.naming.environment, local.env_location.locations_abbreviation, "vnet", local.naming.nn])
+   resource_group_name  = (lookup(each.value, "vnet_resource_group", null) != null && lookup(each.value, "vnet_resource_group", "") != "") ? each.value.vnet_resource_group : join("-", [local.naming.bu, local.naming.environment, local.env_location.locations_abbreviation, "spokenetwork-rg"])
+  
 }
-#magic___^_^___line
-$spokenetworkName = $bu + "-" + $env + "-" + $region + "-spokenetwork-rg"
-Write-Output "SPOKENETWORK NAME: $spokenetworkName"
-#magic___^_^___line
-$vnetName = $bu + "-" + $env + "-" + $region + "-vnet-" + $nn
-Write-Output "VNET NAME: $vnetName"
-#magic___^_^___line
-$subnetName = "lz" + $bu + "-" + $env + "-" + $region + "-" + "${{inputs.purpose}}" + "-snet-${{inputs.subnetNumber}}"
-$subnetName_Lowercase = $subnetName.ToLower()
-Write-Output "SUBNET NAME: $subnetName_Lowercase"
-#magic___^_^___line
-$vnet = Get-AzVirtualNetwork -Name $vnetName -ResourceGroupName $spokenetworkName
-#magic___^_^___line
-if ("${{inputs.action}}" -eq "Add") {
-  Write-Output "==> Adding Subnet"
-  #magic___^_^___line
-  $subnetConfig = Add-AzVirtualNetworkSubnetConfig -Name $subnetName_Lowercase -AddressPrefix "10.0.1.0/24" -VirtualNetwork $vnet
-  Set-AzVirtualNetwork -VirtualNetwork $vnet
-  Write-Output "==> Subnet Added: $subnetName_Lowercase"
-  #magic___^_^___line
-} elseif ("${{inputs.action}}" -eq "Remove") {
-  Write-Output "==> Removing Subnet"
-  #magic___^_^___line
-  $subnetConfig = Remove-AzVirtualNetworkSubnetConfig -Name $subnetName_Lowercase -VirtualNetwork $vnet
-  Set-AzVirtualNetwork -VirtualNetwork $vnet
-  Write-Output "==> Subnet Removed: $subnetName_Lowercase"
-  #magic___^_^___line
-} else {
-  throw "Invalid action. Please specify either 'Add' or 'Remove'."
+output "virtual_network_id" {
+  value = data.azurerm_virtual_network.vnet
+  sensitive = true
 }
-#magic___^_^___line
-$data = @{
-    "geo"                   = $geo
-    "location"              = $region
-    "VNetName"              = $vnetName
-    "SubnetName"            = $subnetName_Lowercase
-    "subnetSize"            = "${{inputs.subnetSize}}"
-    "SubscriptionName"      = $subscriptionName
-    "resourceGroupName"     = $spokenetworkName
-    "requesterEmailAddress" = "${{inputs.adtEmailAddress}}"
+
+
+data "azurerm_subnet" "subnet" {
+  for_each = { for inst in local.get_data : inst.unique_id => inst }
+  name                 = (lookup(each.value, "subnet_name", null) != null && lookup(each.value, "subnet_name", "") != "") ? each.value.subnet_name : var.subnetname //lz<app>-<env>-<region>-<purpose>-snet-<nn>
+  virtual_network_name = data.azurerm_virtual_network.vnet[each.key].name
+  resource_group_name  = (lookup(each.value, "vnet_resource_group", null) != null && lookup(each.value, "vnet_resource_group", "") != "") ? each.value.vnet_resource_group : join("-", [local.naming.bu, local.naming.environment, local.env_location.locations_abbreviation, "spokenetwork-rg"])
 }
-#magic___^_^___line
-$spAppId = Get-AzKeyVaultSecret -VaultName 'ng-prd-eus2-kv-01' -Name "PLAT-logicAppAPI-prod-01-SPN-appId" -AsPlainText
-$Password = Get-AzKeyVaultSecret -VaultName 'ng-prd-eus2-kv-01' -Name "PLAT-logicAppAPI-prod-01-SPN-Password"
-$credential = New-Object System.Management.Automation.PsCredential($spAppId, $Password.SecretValue)
-Connect-AzAccount -ServicePrincipal -Credential $credential -Tenant $tenantId
-$accessToken = Get-AzAccessToken -ResourceUrl 'https://management.core.windows.net/'
-#magic___^_^___line
-$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$headers.Add("Authorization", "Bearer " + $accessToken.Token)
-$headers.Add("Content-Type", "application/json")
-$json = $data | ConvertTo-Json
-Write-Host "==> Sent"
-Write-Host $json
-Write-Host "==> EOM"
-#magic___^_^___line
-$logicAppInfo = Invoke-RestMethod 'https://prod-24.eastus2.logic.azure.com:443/workflows/66492a0351fe422ea527a48f797a59d4/triggers/manual/paths/invoke?api-version=2016-10-01' -Method 'POST' -Headers $headers -Body $json
-$logicAppInfo | ConvertTo-Json
-Write-Host "==> Received"
-Write-Host $logicAppInfo
-Write-Host "==> EOM"
+
+output "subnet_id" {
+  value = data.azurerm_subnet.subnet
+  sensitive = true
+}
+
+resource "azurerm_mssql_server" "mssqlserver" {
+  for_each = { for inst in local.get_data : inst.unique_id => inst }
+   //<bu>-<env>-<region>-<purpose>-sql-<nn>
+  name                         = join("", [local.naming.bu, "-", local.naming.environment, "-", local.env_location.locations_abbreviation, "-", local.purpose, "-sql-",random_id.randomnumber.hex]) 
+  resource_group_name          =  (lookup(each.value,"RGname",null) != null && lookup(each.value,"RGname","") != "" && var.RGname!="") ? each.value.RGname : data.azurerm_resource_group.rg[each.key].name
+  location                     =  var.location
+  version                      =  var.dbserverversion
+  #  administrator_login          = data.azurerm_subscription.current.display_name
+  #  administrator_login_password = random_password.randompassword.result
+  minimum_tls_version          = var.tlsversion
+ public_network_access_enabled = false 
+  azuread_administrator {
+    azuread_authentication_only = true
+    login_username = data.azurerm_subscription.current.display_name
+    object_id      = data.azurerm_client_config.clientconfig.object_id
+  }
+
+  # identity {
+  #   type         = "UserAssigned"
+  #   identity_ids = [azurerm_user_assigned_identity.UserAssigned[each.key].id]
+  # }
+}
+
+# resource "azurerm_mysql_active_directory_administrator" "adadmin" {
+#   for_each = { for inst in local.get_data : inst.unique_id => inst }
+#   server_name         = azurerm_mssql_server.mssqlserver[each.key].name
+#   resource_group_name = (lookup(each.value,"RGname",null) != null && lookup(each.value,"RGname","") != "" && var.RGname!="") ? each.value.RGname : data.azurerm_resource_group.rg[each.key].name
+#   login               = data.azurerm_subscription.current.display_name
+#   tenant_id           = data.azurerm_client_config.clientconfig.tenant_id
+#   object_id           = data.azurerm_client_config.clientconfig.object_id
+#   depends_on = [ azurerm_mssql_server.mssqlserver ]
+# }
+
+resource "azurerm_user_assigned_identity" "UserAssigned" {
+  for_each = { for inst in local.get_data : inst.unique_id => inst }
+
+  location            = var.location
+  name                = join("", [local.naming.bu, "-", local.naming.environment, "-", local.env_location.locations_abbreviation, "-", local.purpose, "-admin"]) 
+  resource_group_name = data.azurerm_resource_group.rg[each.key].name
+}
+
+# Set the TDE option with azure managed keys
+
+resource "azurerm_mssql_server_transparent_data_encryption" "tde" {
+  for_each = { for inst in local.get_data : inst.unique_id => inst }
+  server_id = azurerm_mssql_server.mssqlserver[each.key].id
+  auto_rotation_enabled = true
+}
+
+# enable the traffic between sql server and subnet
+
+# resource "azurerm_mssql_virtual_network_rule" "networkrule" {
+#   for_each = { for inst in local.get_data : inst.unique_id => inst }
+#   name      = "sql-vnet-rule"
+#   server_id = azurerm_mssql_server.mssqlserver[each.key].id 
+#   subnet_id =  data.azurerm_subnet.subnet[each.key].id
+
+#   depends_on = [ azurerm_mssql_server.mssqlserver  ]
+# }
+
+
+# resource "random_password" "randompassword" {
+#  length = 16
+#  special = true
+#  override_special = "!#$%&*()-_=+[]{}<>:?"
+# }
+
+# output "random_pwd" {
+
+#   value = random_password.randompassword
+#   sensitive = true
+# }
+
+resource "random_id" "randomnumber" {
+  keepers = {
+    # time = "${timestamp()}"
+    min = 00
+    max = 99
+  } 
+  byte_length = 1
+}
+
+//***************** Create database *************
+
+resource "azurerm_mssql_database" "sqldb" {
+  for_each = { for inst in local.get_data : inst.unique_id => inst }
+  name           = join("", [local.naming.bu, "-", local.naming.environment, "-", local.env_location.locations_abbreviation, "-", local.purpose, "-sqldb-",random_id.randomnumber.hex]) 
+  server_id      = azurerm_mssql_server.mssqlserver[each.key].id
+  collation      = var.collation//"SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  # max_size_gb    = 4
+  # read_scale     = true
+  sku_name       = var.skuname
+  zone_redundant = var.zoneredundancy
+depends_on = [ azurerm_mssql_server.mssqlserver ]
+}
+
+
+//****************** Failover group Creation/Configuration ********************
+//****************** First create secondary database ************************
+
+# resource "azurerm_mssql_server" "mssqlserversecondary" {
+#   for_each = { for inst in local.get_data : inst.unique_id => inst }
+#    //<bu>-<env>-<region>-<purpose>-sql-<nn>
+#   name                         = join("", [local.naming.bu, "-", local.naming.environment, "-", local.env_location.locations_abbreviation, "-", local.purpose, "-sql-",random_id.randomnumber.hex,"-secondary"]) 
+#   resource_group_name          =  (lookup(each.value,"RGname",null) != null && lookup(each.value,"RGname","") != "" && var.RGname!="") ? each.value.RGname : data.azurerm_resource_group.rg[each.key].name
+#   location                     =  var.location
+#   version                      =  var.dbserverversion
+#   #  administrator_login          = data.azurerm_subscription.current.display_name
+#   #  administrator_login_password = random_password.randompassword.result
+#  minimum_tls_version          = var.tlsversion
+#  public_network_access_enabled = false 
+#   azuread_administrator {
+#     azuread_authentication_only = true
+#     login_username = data.azurerm_subscription.current.display_name
+#     object_id      = data.azurerm_client_config.clientconfig.object_id
+#   }
+
+#   depends_on = [ azurerm_mssql_database.sqldb ]
+# }
+
+# //********** Create Failover group ********************
+
+# resource "azurerm_mssql_failover_group" "failover" {
+#   for_each = { for inst in local.get_data : inst.unique_id => inst }
+#   name      = join("", [local.naming.bu, "-", local.naming.environment, "-", local.env_location.locations_abbreviation, "-", local.purpose, "-sql-",random_id.randomnumber.hex,"-failover-group"]) 
+#   server_id = azurerm_mssql_server.mssqlserver[each.key].id
+#   databases = [
+#     azurerm_mssql_database.sqldb[each.key].id
+#   ]
+
+#   partner_server {
+#     id = azurerm_mssql_server.mssqlserversecondary[each.key].id
+#   }
+
+#   read_write_endpoint_failover_policy {
+#     mode          = "Automatic"
+#     grace_minutes = 60
+#   }
+# depends_on = [ azurerm_mssql_server.mssqlserversecondary ]
+# }
+
+
+# ****************************** Create Private End point ********************
+
+resource "azurerm_private_endpoint" "privateendpoint" {
+  for_each = { for inst in local.get_data : inst.unique_id => inst }
+  name                = join("", [local.naming.bu, "-", local.naming.environment, "-", local.env_location.locations_abbreviation, "-", local.purpose, "-sql-",random_id.randomnumber.hex,"-PE"]) 
+  location            = data.azurerm_resource_group.rg[each.key].location
+  resource_group_name = (lookup(each.value,"RGname",null) != null && lookup(each.value,"RGname","") != "" && var.RGname!="") ? each.value.RGname : data.azurerm_resource_group.rg[each.key].name
+  subnet_id           = data.azurerm_subnet.subnet[each.key].id
+
+  private_service_connection {
+    name                           = join("", [local.naming.bu, "-", local.naming.environment, "-", local.env_location.locations_abbreviation, "-", local.purpose, "-sql-",random_id.randomnumber.hex,"-PSconnection"]) 
+    private_connection_resource_id = azurerm_mssql_server.mssqlserver[each.key].id
+    subresource_names              = ["sqlServer"]
+    is_manual_connection           = false
+  }
+
+  # private_dns_zone_group {
+  #   name                 = "example-dns-zone-group"
+  #   private_dns_zone_ids = [azurerm_private_dns_zone.example.id]
+  # }
+}
+
+# resource "azurerm_private_dns_zone" "example" {
+#   name                = "privatelink.blob.core.windows.net"
+#   resource_group_name = azurerm_resource_group.example.name
+# }
+
+# resource "azurerm_private_dns_zone_virtual_network_link" "example" {
+#   name                  = "example-link"
+#   resource_group_name   = azurerm_resource_group.example.name
+#   private_dns_zone_name = azurerm_private_dns_zone.example.name
+#   virtual_network_id    = azurerm_virtual_network.example.id
+# }
