8c0a0b597cbf4e54c782c75f04db316d2d4ce9e4
kaysplit07 <62314720+kaysplit07@users.noreply.github.com>
2024-11-12 21:15:16 -0600

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,9 +1,6 @@
- Error: Invalid index
-│ 
-│   on main.tf line 134, in resource "azurerm_network_interface_backend_address_pool_association" "lb_backend_association":
-│  134:   backend_address_pool_id  = azurerm_lb_backend_address_pool.internal_lb_bepool[0].id  # Adjust for your pool if needed
-│     ├────────────────
-│     │ azurerm_lb_backend_address_pool.internal_lb_bepool is object with 1 attribute "1"
-│ 
-│ The given key does not identify an element in this collection value. An
-│ object only supports looking up attributes by name, not by numeric index.
+resource "azurerm_network_interface_backend_address_pool_association" "lb_backend_association" {
+  for_each                 = { for nic_key, nic in data.azurerm_network_interface.nic : nic_key => nic }
+  network_interface_id     = each.value.id
+  ip_configuration_name    = "ipconfig1"  # Ensure this matches the actual configuration name of your NICs
+  backend_address_pool_id  = azurerm_lb_backend_address_pool.internal_lb_bepool[each.key].id
+}
