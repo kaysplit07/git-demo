8a138bb9c3763aa676849a172ca65fe105404880
kaysplit07 <62314720+kaysplit07@users.noreply.github.com>
2024-11-12 20:32:13 -0600

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,9 @@
-resource "azurerm_network_interface_backend_address_pool_association" "lb_backend_association" {
-  for_each                 = azurerm_lb_backend_address_pool.internal_lb_bepool
-  network_interface_id     = data.azurerm_network_interface.nic.id  # Assuming a single NIC; adjust if multiple.
-  ip_configuration_name    = "ipconfig1"  # Update this if your NIC uses a different IP configuration name
-  backend_address_pool_id  = each.value.id
-}
+│ Error: IP Configuration "ipconfig1" was not found on Network Interface (Subscription: "***"
+│ Resource Group Name: "6425-DEV-EUS2-SGS-RG"
+│ Network Interface Name: "AZUSE-ACRIDV05-nic-01")
+│ 
+│   with azurerm_network_interface_backend_address_pool_association.lb_backend_association["1"],
+│   on main.tf line 113, in resource "azurerm_network_interface_backend_address_pool_association" "lb_backend_association":
+│  113: resource "azurerm_network_interface_backend_address_pool_association" "lb_backend_association" {
+│ 
+╵
