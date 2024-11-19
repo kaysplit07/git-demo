905ad412bc1749b7bb55d2f79cc0c86d61e6b9e3
kaysplit07 <62314720+kaysplit07@users.noreply.github.com>
2024-11-18 23:13:36 -0600

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 name: 'Deploy Load Balancer'
-run-name: 'Load Balancer - ${{inputs.environment}} purpose: ${{inputs.purpose}} : ${{inputs.requesttype}}'
+run-name: 'Load Balancer - ${{ inputs.environment }} Purpose: ${{ inputs.purpose }} : ${{ inputs.requesttype }}'
 on:
   workflow_dispatch:
     inputs:
@@ -8,9 +8,9 @@ on:
         required: true
         description: Request Type
         options:
-            - Create (with New RG)
-            - Create (with Existing RG)
-            - Remove
+          - Create (with New RG)
+          - Create (with Existing RG)
+          - Remove
         default: "Create (with New RG)"
       environment:
         type: choice
@@ -44,89 +44,63 @@ on:
         required: true
         description: Purpose of the Load Balancer
       RGname:
-          type: string
-          required: false
+        type: string
+        required: false
       purposeRG:
         type: string
         required: true
-        description: Resource Group Purpose.......... Hyphen designate an existing RG
+        description: Resource Group Purpose (hyphen designates an existing RG)
       subnetname:
         type: string
         required: true
-        description: Subnet name for Load Balancer.
+        description: Subnet name for Load Balancer
       private_ip_address:
         type: string
         required: false
-        description: Private IP address for Load Balancer frontend configuration.
-      vm_names:
-        description: "List of VM names for NIC IP configuration"
+        description: Private IP address for Load Balancer frontend configuration
+      vm_list:
+        type: string
         required: false
-        default: '["VM1", "VM2", "VM3"]'
+        description: "List of VMs for NIC IP configuration (as JSON)"
 jobs:
   resource_group:
-   #if: (github.event.inputs.requesttype == 'Create (with New RG)')
-    if: ${{ inputs.vm_list == null && (inputs.requestType == 'Create (with New RG)' || inputs.requestType == 'Create (with Existing RG)') }}
-    name: 'Resource Group ${{inputs.purposeRG}}'
+    if: ${{ inputs.requesttype == 'Create (with New RG)' }}
+    name: 'Resource Group ${{ inputs.purposeRG }}'
     uses: ./.github/workflows/CreateResourceGroup.yml
     secrets: inherit
     with:
-      name:                 'resource-group'
-      subscription:         'SNow Input'
-      environment:          '${{inputs.environment}}' 
-      location:             '${{inputs.location}}' 
-      purpose:              '${{inputs.purposeRG}}'
-  
-  load_balancer_new_rg:
-    #if: (github.event.inputs.requesttype == 'Create (with New RG)')
-    if: ${{ inputs.vm_list == null && (inputs.requestType == 'Create (with New RG)' || inputs.requestType == 'Create (with Existing RG)') }}
-    name: 'Load Balancer ${{inputs.purpose}}'
+      name: 'resource-group'
+      subscription: 'Subscription Input'
+      environment: '${{ inputs.environment }}'
+      location: '${{ inputs.location }}'
+      purpose: '${{ inputs.purposeRG }}'
+
+  load_balancer:
+    if: ${{ inputs.requesttype != 'Remove' }}
+    name: 'Load Balancer ${{ inputs.purpose }}'
     uses: ./.github/workflows/LBCreate.yml
     needs: resource_group
     secrets: inherit
     with:
-      requesttype:               '${{inputs.requesttype}}'
-      environment:               '${{inputs.environment}}'
-      location:                  '${{inputs.location}}'
-      sku_name:                  '${{inputs.sku_name}}'
-      purpose:                   '${{inputs.purpose}}'
-      RGname:                    '${{inputs.RGname}}'
-      purposeRG:                 '${{inputs.purposeRG}}'
-      subnetname:                '${{inputs.subnetname}}'
-      private_ip_address:        '${{inputs.private_ip_address}}'
-     # vm_names:                  '${{inputs.vm_names}}'
-     vm_list: '[{"vm_name": "VM1", "nic_name": "NIC1"}, {"vm_name": "VM2", "nic_name": "NIC2"}]'
-  load_balancer_existing_rg:
-    if: (github.event.inputs.requesttype == 'Create (with Existing RG)')
-    name: 'Load Balancer ${{inputs.purpose}}'
-    uses: ./.github/workflows/LBCreate.yml
-    secrets: inherit
-    with:
-      requesttype:               '${{inputs.requesttype}}'
-      environment:               '${{inputs.environment}}'
-      location:                  '${{inputs.location}}'
-      sku_name:                  '${{inputs.sku_name}}'
-      purpose:                   '${{inputs.purpose}}'
-      purposeRG:                 '${{inputs.purposeRG}}'
-      subnetname:                '${{inputs.subnetname}}'
-      RGname:                    '${{inputs.RGname}}'
-      private_ip_address:        '${{inputs.private_ip_address}}'
-      #vm_names:                  '${{inputs.vm_names}}'
-      vm_list: '[{"vm_name": "VM1", "nic_name": "NIC1"}, {"vm_name": "VM2", "nic_name": "NIC2"}]'
-  
+      requesttype: '${{ inputs.requesttype }}'
+      environment: '${{ inputs.environment }}'
+      location: '${{ inputs.location }}'
+      sku_name: '${{ inputs.sku_name }}'
+      purpose: '${{ inputs.purpose }}'
+      RGname: '${{ inputs.RGname }}'
+      purposeRG: '${{ inputs.purposeRG }}'
+      subnetname: '${{ inputs.subnetname }}'
+      private_ip_address: '${{ inputs.private_ip_address }}'
+      vm_list: >
+        [{"vm_name": "VM1", "nic_name": "NIC1"}, {"vm_name": "VM2", "nic_name": "NIC2"}]
+
   load_balancer_remove:
-    if: (github.event.inputs.requesttype == 'Remove')
-    name: 'Maintain Load Balancer ${{inputs.purpose}}'
+    if: ${{ inputs.requesttype == 'Remove' }}
+    name: 'Remove Load Balancer ${{ inputs.purpose }}'
     uses: ./.github/workflows/LBCreate.yml
     secrets: inherit
     with:
-      requesttype:               '${{inputs.requesttype}}'
-      environment:               '${{inputs.environment}}'
-      location:                  '${{inputs.location}}'
-      sku_name:                  '${{inputs.sku_name}}'
-      purpose:                   '${{inputs.purpose}}'
-      purposeRG:                 '${{inputs.purposeRG}}'
-      RGname:                    '${{inputs.RGname}}'
-      subnetname:                '${{inputs.subnetname}}'
-      private_ip_address:        '${{inputs.private_ip_address}}'
-      #vm_names:                  '${{inputs.vm_names}}'
-      vm_list: '[{"vm_name": "VM1", "nic_name": "NIC1"}, {"vm_name": "VM2", "nic_name": "NIC2"}]'
+      requesttype: '${{ inputs.requesttype }}'
+      environment: '${{ inputs.environment }}'
+      location: '${{ inputs.location }}'
+     
