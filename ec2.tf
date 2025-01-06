b4405935507565b5abb40dd3645c5f5e6690d039
kaysplit07 <62314720+kaysplit07@users.noreply.github.com>
2025-01-05 18:40:33 -0600

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,27 +1,89 @@
-# Variable to receive the load balancer backend pool ID from the load balancer module
-variable "loadbalancer_backend_address_pool_id" {
-  description = "ID of the load balancer backend address pool"
-  type        = map(string)
-}
 
-# Network Interface Resource
-resource "azurerm_network_interface" "nic" {
-  provider = azurerm.adt
-  for_each = { for row_id, inst in local.final_parms_map : row_id => inst }
-  location            = local.naming.location
-  name                = (each.value).nic_name
-  resource_group_name = (each.value).rg_name
 
-  ip_configuration {
-    name                          = join("-", [(each.value).vm_name, "nic_config"])
-    subnet_id                     = (each.value).nic_subnet_id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
+Error
+No event triggers defined in `on`
 
-# Backend Address Pool Association Resource
-resource "azurerm_network_interface_backend_address_pool_association" "VM_to_LB" {
-  for_each                         = azurerm_network_interface.nic
-  network_interface_id             = each.value.id
-  backend_address_pool_id          = var.loadbalancer_backend_address_pool_id[each.key]
-}
+name: 'NIC Association to LB '
+run-name: 'NIC Association - ${{inputs.environment}} purpose: ${{inputs.purpose}} : ${{inputs.requesttype}}'
+on:
+  workflow_dispatch:
+    inputs:
+      requesttype:
+        type: choice
+        required: true
+        description: Request Type
+        options:
+            - NIC Association to LB
+            - Remove NIC from LB
+      environment:
+        type: choice
+        required: true
+        description: Environment
+        options:
+          - DEV       
+          - UAT
+          - QA
+          - PROD
+      location:
+        type: choice
+        required: true
+        description: Deployment Location
+        options:
+          - Select the location
+          - eastus2
+          - uksouth
+          - centralus
+          - ukwest
+      purpose:
+        type: string
+        required: true
+        description: Purpose of the NIC
+      purposeRG:
+        type: string
+        required: false
+        default: "sgs"
+        description: Resource Group Purpose..........
+      nic_resource_group:
+        type: string
+        required: false
+        description: resource group name for NIC.
+      nic_names:
+        description: "Comma-separated list of NIC names"
+        required: true
+        default: "nic1,nic2,nic3"
+      lb_name:
+        description: "Load Balancer Name to Attache the NIC"
+        required: true
+        default: "lb1"
+
+jobs:
+  NICAssociation:
+      if: (github.event.inputs.requesttype == 'NIC Association to LB')
+      name: 'NIC to LB ${{inputs.purpose}}'
+      uses: ./.github/workflows/NICAssociation.yml
+      secrets: inherit
+      with:
+        requesttype:                 '${{inputs.requesttype}}'
+        environment:                 '${{inputs.environment}}'
+        location:                    '${{inputs.location}}'
+        purpose:                     '${{inputs.purpose}}'
+        purposeRG:                   '${{inputs.purposeRG}}'
+        nic_resource_group:          '${{inputs.nic_resource_group}}'
+        nic_names:                   '${{inputs.nic_names}}'
+        lb_name:                    '${{inputs.lb_name}}'  
+
+  RemoveNICAssociation:
+      if: (github.event.inputs.requesttype == 'Remove NIC from LB')
+      name: 'NIC to LB ${{inputs.purpose}}'
+      uses: ./.github/workflows/NICAssociation.yml
+      secrets: inherit
+      with:
+        requesttype:                 '${{inputs.requesttype}}'
+        environment:                 '${{inputs.environment}}'
+        location:                    '${{inputs.location}}'
+        purpose:                     '${{inputs.purpose}}'
+        purposeRG:                   '${{inputs.purposeRG}}'
+        nic_resource_group:          '${{inputs.nic_resource_group}}'
+        nic_names:                   '${{inputs.nic_names}}'
+        lb_name:                    '${{inputs.lb_name}}'  
+  
