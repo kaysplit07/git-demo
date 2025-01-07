9c6f6ff501a90386ebb26c6be7c61fd968243afc
kaysplit07 <62314720+kaysplit07@users.noreply.github.com>
2025-01-07 17:15:50 -0600

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,5 @@
-Invalid workflow file: .github/workflows/mysql_server.yml#L84
-error parsing called workflow
-".github/workflows/mysql_server.yml"
--> "./.github/workflows/Createmssqlserver.yml" (source branch with sha:e2fd7c1b6745760718ff072c752fabe29d748c48)
-: You have an error in your yaml syntax on line 53
+Invalid workflow file: .github/workflows/mysql_server.yml#L74
+The workflow is not valid. .github/workflows/mysql_server.yml (Line: 74, Col: 34): Invalid secret, BACKEND_STORAGE_ACCOUNT is not defined in the referenced workflow. .github/workflows/mysql_server.yml (Line: 75, Col: 34): Invalid secret, BACKEND_RESOURCE_GROUP is not defined in the referenced workflow.
 
 ###################
 
@@ -63,7 +60,9 @@ jobs:
         ARM_CLIENT_SECRET: ${{secrets.ARM_CLIENT_SECRET}}
         ARM_TENANT_ID: ${{secrets.ARM_TENANT_ID}}
         ARM_SUBSCRIPTION_ID: ${{secrets.ARM_SUBSCRIPTION_ID}}
-        ROOT_PATH: 'Azure/Azure-LB
+        BACKEND_STORAGE_ACCOUNT: ${{secrets.BACKEND_STORAGE_ACCOUNT}}
+        BACKEND_RESOURCE_GROUP: ${{secrets.BACKEND_RESOURCE_GROUP}}
+        ROOT_PATH: 'Azure/Azure-LB'
     runs-on: 
       group: aks-runners
     environment: ${{inputs.environment}}
@@ -168,3 +167,151 @@ jobs:
         TF_VAR_BACKEND_STORAGE_ACCOUNT: ${{secrets.BACKEND_STORAGE_ACCOUNT}}
         TF_VAR_BACKEND_RESOURCE_GROUP:  ${{secrets.BACKEND_RESOURCE_GROUP}}
     
+########################
+
+name: 'Deploy MSSQL Server'
+run-name: '${{github.actor}} - Deployingto_${{inputs.subscription}}_${{inputs.environment}}'
+on:
+    workflow_dispatch:
+     inputs:
+        requesttype:
+          type: choice
+          required: true
+          description: Request Type
+          options:
+            - Create (with New RG)
+            - Create (with Existing RG)
+            - Remove (Destroy SQL)
+          default: "Create (with New RG)"
+        subscription:
+          type: string
+          required: true
+          description: Please enter your subcription Name
+        location:
+          type: choice
+          description: Pick the Location
+          options:
+            - eastus2
+            - centralus
+        environment:
+          type: choice
+          description: choose the environment
+          options:
+             - dev
+             - qa 
+             - UAT
+             - Prod
+        purpose:
+          type: string
+          required: true
+          description: Enter Purpose for app (3-5 char)
+        subnetname:
+          type: string
+          required: true
+          description: Enter the subnet name for db end points
+        dbcollation:
+          type: string
+          required: false
+          description: Specify Collation of the database
+          default: SQL_Latin1_General_CP1_CI_AS
+        skuname:
+          type: choice
+          description: select SKU_NAME used by Database
+          options:
+            - S0
+            - P2
+            - Basic
+            - ElasticPool
+            - BC_Gen5_2
+            - HS_Gen4_1
+            - GP_S_Gen5_2
+            - DW100c
+            - DS100
+        zoneredundancy:
+          type: choice
+          options:
+            - "false"
+            - "true"
+jobs:
+  Deploying-Resource-Group:
+    if: ${{ inputs.requesttype == 'Create (with New RG)' }}
+    name: 'Deploying - resource-group'
+    uses: ./.github/workflows/CreateResourceGroup.yml
+    secrets:
+      ARM_CLIENT_ID: ${{secrets.AZURE_CLIENT_ID}}
+      ARM_CLIENT_SECRET: ${{secrets.AZURE_CLIENT_SECRET}}
+      ARM_SUBSCRIPTION_ID: ${{secrets.AZURE_SUBSCRIPTION_ID}}
+      ARM_TENANT_ID: ${{secrets.AZURE_TENANT_ID}}
+      BACKEND_STORAGE_ACCOUNT:   ${{secrets.BACKEND_STORAGE_ACCOUNT}}
+      BACKEND_RESOURCE_GROUP:    ${{secrets.BACKEND_RESOURCE_GROUP}}
+    with:
+      name: 'resource-group'
+      subscription: '${{inputs.subscription}}'
+      location: '${{inputs.location}}'
+      environment: '${{inputs.environment}}'
+      purpose: '${{inputs.purpose}}'
+  Deploying-Mysql-server:
+    name: 'Deploying - MSSQL server'
+    uses: ./.github/workflows/Createmssqlserver.yml
+    needs: Deploying-Resource-Group
+    secrets: 
+      ARM_CLIENT_ID: ${{secrets.AZURE_CLIENT_ID}}
+      ARM_CLIENT_SECRET: ${{secrets.AZURE_CLIENT_SECRET}}
+      ARM_SUBSCRIPTION_ID: ${{secrets.AZURE_SUBSCRIPTION_ID}}
+      ARM_TENANT_ID: ${{secrets.AZURE_TENANT_ID}}
+      BACKEND_STORAGE_ACCOUNT:   ${{secrets.BACKEND_STORAGE_ACCOUNT}}
+      BACKEND_RESOURCE_GROUP:    ${{secrets.BACKEND_RESOURCE_GROUP}}
+    with:
+      name: 'mssql'
+      subscription: '${{inputs.subscription}}'
+      location: '${{inputs.location}}'
+      environment: '${{inputs.environment}}'
+      purpose: '${{inputs.purpose}}'
+      subnetname: '${{inputs.subnetname}}'
+      dbcollation: '${{inputs.dbcollation}}'
+      skuname: '${{inputs.skuname}}'
+      zoneredundancy: '${{inputs.zoneredundancy}}'
+      
+  Deploying-Mssql-server-existing-rg:
+    if: ${{ inputs.requesttype == 'Create (with Existing RG)' }}
+    name: 'Deploying - MSSQL Server (Existing RG)'
+    uses: ./.github/workflows/Createmssqlserver.yml
+    secrets:
+      ARM_CLIENT_ID: ${{ secrets.AZURE_CLIENT_ID }}
+      ARM_CLIENT_SECRET: ${{ secrets.AZURE_CLIENT_SECRET }}
+      ARM_SUBSCRIPTION_ID: ${{ secrets.AZURE_SUBSCRIPTION_ID }}
+      ARM_TENANT_ID: ${{ secrets.AZURE_TENANT_ID }}
+      BACKEND_STORAGE_ACCOUNT:   ${{secrets.BACKEND_STORAGE_ACCOUNT}}
+      BACKEND_RESOURCE_GROUP:    ${{secrets.BACKEND_RESOURCE_GROUP}}
+    with:
+      name: 'mssql'
+      subscription: '${{ inputs.subscription }}'
+      location: '${{ inputs.location }}'
+      environment: '${{ inputs.environment }}'
+      purpose: '${{ inputs.purpose }}'
+      subnetname: '${{ inputs.subnetname }}'
+      dbcollation: '${{ inputs.dbcollation }}'
+      skuname: '${{ inputs.skuname }}'
+      zoneredundancy: '${{ inputs.zoneredundancy }}'
+
+  Removing-Mssql-server:
+    if: ${{ inputs.requesttype == 'Remove (Destroy SQL)' }}
+    name: 'Removing - MSSQL Server'
+    uses: ./.github/workflows/Createmssqlserver.yml
+    secrets:
+      ARM_CLIENT_ID: ${{ secrets.AZURE_CLIENT_ID }}
+      ARM_CLIENT_SECRET: ${{ secrets.AZURE_CLIENT_SECRET }}
+      ARM_SUBSCRIPTION_ID: ${{ secrets.AZURE_SUBSCRIPTION_ID }}
+      ARM_TENANT_ID: ${{ secrets.AZURE_TENANT_ID }}
+      BACKEND_STORAGE_ACCOUNT:   ${{secrets.BACKEND_STORAGE_ACCOUNT}}
+      BACKEND_RESOURCE_GROUP:    ${{secrets.BACKEND_RESOURCE_GROUP}}
+    with:
+      name: 'mssql'
+      subscription: '${{ inputs.subscription }}'
+      location: '${{ inputs.location }}'
+      environment: '${{ inputs.environment }}'
+      purpose: '${{ inputs.purpose }}'
+      subnetname: '${{ inputs.subnetname }}'
+      dbcollation: '${{ inputs.dbcollation }}'
+      skuname: '${{ inputs.skuname }}'
+      zoneredundancy: '${{ inputs.zoneredundancy }}'
