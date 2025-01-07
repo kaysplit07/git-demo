eaede9cf44b1193cff7f1a290abbbae1fbfb64a9
kaysplit07 <62314720+kaysplit07@users.noreply.github.com>
2025-01-06 20:54:24 -0600

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,101 @@
-name: 'NIC Association to LB'
-run-name: 'NIC Association - ${{ inputs.environment }} purpose: ${{ inputs.purpose }} : ${{ inputs.requesttype }}'
+name: 'Deploy MSSQL Server'
+run-name: '${{github.actor}} - Deployingto_${{inputs.subscription}}_${{inputs.environment}}'
+on:
+    workflow_dispatch:
+     inputs:
+        subscription:
+          type: string
+          required: true
+          description: Please enter your subcription Name
+        location:
+          type: choice
+          description: Pick the Location
+          options:
+            - eastus2
+            - centralus
+        environment:
+          type: choice
+          description: choose the environment
+          options:
+             - dev
+             - qa 
+             - UAT
+             - Prod
+        purpose:
+          type: string
+          required: true
+          description: Enter Purpose for app (3-5 char)
+        subnetname:
+          type: string
+          required: true
+          description: Enter the subnet name for db end points
+        dbcollation:
+          type: string
+          required: false
+          description: Specify Collation of the database
+          default: SQL_Latin1_General_CP1_CI_AS
+        skuname:
+          type: choice
+          description: select SKU_NAME used by Database
+          options:
+            - S0
+            - P2
+            - Basic
+            - ElasticPool
+            - BC_Gen5_2
+            - HS_Gen4_1
+            - GP_S_Gen5_2
+            - DW100c
+            - DS100
+        zoneredundancy:
+          type: choice
+          options:
+            - "false"
+            - "true"
+jobs:
+  Deploying-Resource-Group:
+    name: 'Deploying - resource-group'
+    uses: ./.github/workflows/CreateResourceGroup.yml
+    secrets:
+      ARM_CLIENT_ID: ${{secrets.AZURE_CLIENT_ID}}
+      ARM_CLIENT_SECRET: ${{secrets.AZURE_CLIENT_SECRET}}
+      ARM_SUBSCRIPTION_ID: ${{secrets.AZURE_SUBSCRIPTION_ID}}
+      ARM_TENANT_ID: ${{secrets.AZURE_TENANT_ID}}
+
+    with:
+      name: 'resource-group'
+      subscription: '${{inputs.subscription}}'
+      location: '${{inputs.location}}'
+      environment: '${{inputs.environment}}'
+      purpose: '${{inputs.purpose}}'
+  Deploying-Mysql-server:
+    name: 'Deploying - MSSQL server'
+    uses: ./.github/workflows/Createmssqlserver.yml
+    needs: Deploying-Resource-Group
+    secrets: 
+      ARM_CLIENT_ID: ${{secrets.AZURE_CLIENT_ID}}
+      ARM_CLIENT_SECRET: ${{secrets.AZURE_CLIENT_SECRET}}
+      ARM_SUBSCRIPTION_ID: ${{secrets.AZURE_SUBSCRIPTION_ID}}
+      ARM_TENANT_ID: ${{secrets.AZURE_TENANT_ID}}
+      BACKEND_STORAGE_ACCOUNT: ${{secrets.BACKEND_STORAGE_ACCOUNT}}
+      BACKEND_RESOURCE_GROUP: ${{secrets.BACKEND_RESOURCE_GROUP}}
+    with:
+      name: 'mssql'
+      subscription: '${{inputs.subscription}}'
+      location: '${{inputs.location}}'
+      environment: '${{inputs.environment}}'
+      purpose: '${{inputs.purpose}}'
+      subnetname: '${{inputs.subnetname}}'
+      dbcollation: '${{inputs.dbcollation}}'
+      skuname: '${{inputs.skuname}}'
+      zoneredundancy: '${{inputs.zoneredundancy}}'
+
+
+
+##################
+
+name: '- Deploy Load Balancer'
+run-name: 'Load Balancer - ${{ inputs.environment }} purpose: ${{ inputs.purpose }} : ${{ inputs.requesttype }}'
 
 on:
   workflow_dispatch:
@@ -9,77 +105,119 @@ on:
         required: true
         description: Request Type
         options:
-          - NIC Association to LB
-          - Remove NIC from LB
+            - Create (with New RG)
+            - Create (with Existing RG)
+            - Remove (Destroy LB)
+        default: 
+            "Create (with New RG)"
       environment:
         type: choice
         required: true
         description: Environment
         options:
-          - DEV       
-          - UAT
-          - QA
-          - PROD
+        - DEV
+        - UAT
+        - QA
+        - PROD
       location:
         type: choice
         required: true
         description: Deployment Location
         options:
-          - eastus2
-          - uksouth
-          - centralus
-          - ukwest
-      purpose:
-        type: string
-        required: true
-        description: Purpose of the NIC
+            - Select the location
+            - eastus2
+            - uksouth
+            - centralus
+            - ukwest
       purposeRG:
         type: string
+        required: true
+        description: Resource Group Purpose. Hyphen designates an existing RG
+      sku:
+        type: choice
         required: false
-        default: "sgs"
-        description: Resource Group Purpose
-      nic_resource_group:
+        description: SKU Type for the Load Balancer
+        options:
+            - Basic
+            - Standard
+        default: "Standard"
+      private_ip:
         type: string
         required: false
-        description: Resource group name for NIC
-      nic_names:
+        description: Private IP address for the Load Balancer frontend configuration (if applicable)
+      subnetName:
         type: string
         required: true
-        description: "Comma-separated list of NIC names"
-        default: "nic1,nic2,nic3"
-      lb_name:
-        type: string
-        required: true
-        description: "Load Balancer Name to Attach the NIC"
-        default: "lb1"
+        description: Subnet name for the network interface
 
 jobs:
-  NICAssociation:
-    if: github.event.inputs.requesttype == 'NIC Association to LB'
-    name: 'NIC to LB ${{ inputs.purpose }}'
-    uses: ./.github/workflows/NICAssociation.yml
-    secrets: inherit
+  resource_group:
+    if: (github.event.inputs.requesttype == 'Create (with New RG)')
+    name: 'Resource Group ${{ inputs.purposeRG }}'
+    uses: ./.github/workflows/CreateResourceGroup.yml
+    secrets:
+      ARM_CLIENT_ID: ${{ secrets.AZURE_CLIENT_ID }}
+      ARM_CLIENT_SECRET: ${{ secrets.AZURE_CLIENT_SECRET }}
+      ARM_SUBSCRIPTION_ID: ${{ secrets.AZURE_SUBSCRIPTION_ID }}
+      ARM_TENANT_ID: ${{ secrets.AZURE_TENANT_ID }}
     with:
-      requesttype:                 '${{ inputs.requesttype }}'
-      environment:                 '${{ inputs.environment }}'
-      location:                    '${{ inputs.location }}'
-      purpose:                     '${{ inputs.purpose }}'
-      purposeRG:                   '${{ inputs.purposeRG }}'
-      nic_resource_group:          '${{ inputs.nic_resource_group }}'
-      nic_names:                   '${{ inputs.nic_names }}'
-      lb_name:                     '${{ inputs.lb_name }}'
+      name: 'resource-group'
+      subscription: 'SNow Input'
+      environment: '${{ inputs.environment }}'
+      location: '${{ inputs.location }}'
+      purpose: '${{ inputs.purposeRG }}'
 
-  RemoveNICAssociation:
-    if: github.event.inputs.requesttype == 'Remove NIC from LB'
-    name: 'Remove NIC from LB ${{ inputs.purpose }}'
-    uses: ./.github/workflows/NICAssociation.yml
-    secrets: inherit
+  load_balancer_new_rg:
+    if: (github.event.inputs.requesttype == 'Create (with New RG)')
+    name: 'Load Balancer ${{ inputs.purpose }}'
+    uses: ./.github/workflows/LBCreate.yml
+    needs: resource_group
+    secrets:
+      ARM_CLIENT_ID: ${{ secrets.AZURE_CLIENT_ID }}
+      ARM_CLIENT_SECRET: ${{ secrets.AZURE_CLIENT_SECRET }}
+      ARM_SUBSCRIPTION_ID: ${{ secrets.AZURE_SUBSCRIPTION_ID }}
+      ARM_TENANT_ID: ${{ secrets.AZURE_TENANT_ID }}
     with:
-      requesttype:                 '${{ inputs.requesttype }}'
-      environment:                 '${{ inputs.environment }}'
-      location:                    '${{ inputs.location }}'
-      purpose:                     '${{ inputs.purpose }}'
-      purposeRG:                   '${{ inputs.purposeRG }}'
-      nic_resource_group:          '${{ inputs.nic_resource_group }}'
-      nic_names:                   '${{ inputs.nic_names }}'
-      lb_name:                     '${{ inputs.lb_name }}'
+      requesttype: '${{ inputs.requesttype }}'
+      environment: '${{ inputs.environment }}'
+      location: '${{ inputs.location }}'
+      purposeRG: '${{ inputs.purposeRG }}'
+      sku: '${{ inputs.sku }}'
+      private_ip: '${{ inputs.private_ip }}'
+      subnetName: '${{ inputs.subnetName }}'
+
+  load_balancer_existing_rg:
+    if: (github.event.inputs.requesttype == 'Create (with Existing RG)')
+    name: 'Load Balancer ${{ inputs.purpose }}'
+    uses: ./.github/workflows/LBCreate.yml
+    secrets:
+      ARM_CLIENT_ID: ${{ secrets.AZURE_CLIENT_ID }}
+      ARM_CLIENT_SECRET: ${{ secrets.AZURE_CLIENT_SECRET }}
+      ARM_SUBSCRIPTION_ID: ${{ secrets.AZURE_SUBSCRIPTION_ID }}
+      ARM_TENANT_ID: ${{ secrets.AZURE_TENANT_ID }}
+    with:
+      requesttype: '${{ inputs.requesttype }}'
+      environment: '${{ inputs.environment }}'
+      location: '${{ inputs.location }}'
+      purposeRG: '${{ inputs.purposeRG }}'
+      sku: '${{ inputs.sku }}'
+      private_ip: '${{ inputs.private_ip }}'
+      subnetName: '${{ inputs.subnetName }}'
+
+  load_balancer_remove:
+    if: (github.event.inputs.requesttype == 'Remove (Destroy LB)')
+    name: 'Remove Load Balancer ${{ inputs.purpose }}'
+    uses: ./.github/workflows/LBRemove.yml
+    secrets:
+      ARM_CLIENT_ID: ${{ secrets.AZURE_CLIENT_ID }}
+      ARM_CLIENT_SECRET: ${{ secrets.AZURE_CLIENT_SECRET }}
+      ARM_SUBSCRIPTION_ID: ${{ secrets.AZURE_SUBSCRIPTION_ID }}
+      ARM_TENANT_ID: ${{ secrets.AZURE_TENANT_ID }}
+    with:
+      requesttype: '${{ inputs.requesttype }}'
+      environment: '${{ inputs.environment }}'
+      location: '${{ inputs.location }}'
+      purposeRG: '${{ inputs.purposeRG }}'
+      sku: '${{ inputs.sku }}'
+      private_ip: '${{ inputs.private_ip }}'
+      subnetName: '${{ inputs.subnetName }}'
