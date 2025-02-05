539102e6f73a0758747dcfbcfd1e3443967aceaf
T Akintade <62314720+kaysplit07@users.noreply.github.com>
2025-02-04 19:25:05 -0600

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,5 @@
-kubectl exec -it <pod-name> -n <namespace> -- cat /path/to/certificate.crt | openssl x509 -noout -text
 
-
-
-kubectl exec -it <pod-name> -n <namespace> -- cat /path/to/certificate.crt | openssl x509 -noout -enddate
-
-
-
-
+C5392450@KTWJ124QK9 ~ % kubectl exec -it buyer-cms-content-management-64db69d9f8-hds5v -n family-buyer-cms -- cat /path/to/certificate.crt | openssl x509 -noout -text
+command terminated with exit code 1
+unable to load certificate
+8321322816:error:09FFF06C:PEM routines:CRYPTO_internal:no start line:/AppleInternal/Library/BuildRoots/4ff29661-3588-11ef-9513-e2437461156c/Library/Caches/com.apple.xbs/Sources/libressl/libressl-3.3/crypto/pem/pem_lib.c:694:Expecting: TRUSTED CERTIFICATE
