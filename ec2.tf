acdebe7e208f226619527530115d756da6ab40d9
T Akintade <62314720+kaysplit07@users.noreply.github.com>
2025-02-04 19:57:21 -0600

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,21 @@
+kubectl exec -it buyer-cms-content-management-64db69d9f8-hds5v -n family-buyer-cms -- ls -l /path/to/
 
-C5392450@KTWJ124QK9 ~ % kubectl exec -it buyer-cms-content-management-64db69d9f8-hds5v -n family-buyer-cms -- cat /path/to/certificate.crt | openssl x509 -noout -text
-command terminated with exit code 1
-unable to load certificate
-8321322816:error:09FFF06C:PEM routines:CRYPTO_internal:no start line:/AppleInternal/Library/BuildRoots/4ff29661-3588-11ef-9513-e2437461156c/Library/Caches/com.apple.xbs/Sources/libressl/libressl-3.3/crypto/pem/pem_lib.c:694:Expecting: TRUSTED CERTIFICATE
+
+ Possible locations:
+
+/etc/ssl/certs/
+/etc/nginx/ssl/
+/etc/pki/tls/certs/
+/etc/letsencrypt/live/<domain>/fullchain.pem
+
+kubectl exec -it buyer-cms-content-management-64db69d9f8-hds5v -n family-buyer-cms -- ls -l /etc/ssl/certs/
+
+
+check the content
+
+kubectl exec -it buyer-cms-content-management-64db69d9f8-hds5v -n family-buyer-cms -- cat /path/to/certificate.crt
+
+decode
+
+kubectl exec -it buyer-cms-content-management-64db69d9f8-hds5v -n family-buyer-cms -- sh -c "cat /path/to/certificate.crt | base64 -d | openssl x509 -noout -text"
+
