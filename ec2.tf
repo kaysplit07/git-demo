427dbf722c6d73a9abb025ece60c8c9a3dea68f3
T Akintade <62314720+kaysplit07@users.noreply.github.com>
2025-02-05 10:17:46 -0600

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,21 +1,3 @@
-kubectl exec -it buyer-cms-content-management-64db69d9f8-hds5v -n family-buyer-cms -- ls -l /path/to/
+kubectl exec -it buyer-cms-content-management-64db69d9f8-hds5v -n family-buyer-cms -- sh -c "openssl x509 -in /etc/ssl/certs/example.crt -noout -enddate"
 
-
- Possible locations:
-
-/etc/ssl/certs/
-/etc/nginx/ssl/
-/etc/pki/tls/certs/
-/etc/letsencrypt/live/<domain>/fullchain.pem
-
-kubectl exec -it buyer-cms-content-management-64db69d9f8-hds5v -n family-buyer-cms -- ls -l /etc/ssl/certs/
-
-
-check the content
-
-kubectl exec -it buyer-cms-content-management-64db69d9f8-hds5v -n family-buyer-cms -- cat /path/to/certificate.crt
-
-decode
-
-kubectl exec -it buyer-cms-content-management-64db69d9f8-hds5v -n family-buyer-cms -- sh -c "cat /path/to/certificate.crt | base64 -d | openssl x509 -noout -text"
 
